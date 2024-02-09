e402c652490f760eb682792634a4a1aeaca9ed32
sumanpatel18 <141306994+sumanpatel18@users.noreply.github.com>
2024-02-09 14:54:37 +0530

    committed 2/9/2024, 2:54:36 PM

---
+++ set02/problem01.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void input(float *base, float *height);
+void find_area(float base , float height, float *area);
+void output(float base, float height, float area);
+int main(){
+float base, height, area;
+input(&base,&height);
+find_area(base, height, &area);
+output(base, height, area);
+return 0;
+}
+void input(float *base, float *height){
+    printf("enter the base:");
+    scanf("%f", base);
+    printf("enter the height:");
+    scanf("%f",height);
+}
+void find_area(float base , float height, float *area){
+    *area=0.5*base*height;
+}
+void output(float base, float height, float area)
+{
+    printf("the area with base %f and height %f is %f:",base, height, area);
+}
