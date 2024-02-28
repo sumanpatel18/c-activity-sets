d1983380821c8a7e46c2e70b05d68a30ac940e80
sumanpatel18 <141306994+sumanpatel18@users.noreply.github.com>
2024-02-28 22:44:02 +0530

    committed 2/28/2024, 10:44:02 PM

--- set02/problem04.c
+++ set02/problem04.c
@@ -1,20 +1,51 @@
 ///4)Write a program to find Sum of composite number in an array of different numbers entered by the user.
-
+#include<stdio.h>
 int input_array_size();
 void input_array(int n, int a[n]);
 int sum_composite_numbers(int n, int a[n]);
 void output(int sum);
 int main()
 {
-
+int n = input_array_size();
+int array[n];
+input_array(n, array);
+int sum= sum_composite_numbers(n, array);
+output(sum);
+return 0;
 }
 int input_array_size()
 {
 int n;
 printf("enter the number\n");
 scanf("%d",&n);
-return 0;
+return n;
 }
-void input_array(int n, int a[n]){
-    
+void input_array(int n, int a[n])
+{
+    printf("Enter %d numbers for the array:\n",n);
+    for(int i=0; i<n; i++){
+        printf("Element %d:", i+1);
+        scanf("%d", &a[i]);
+    }
+}
+int is_composite(int num){
+    int factors =0;
+    for(int i=1; i<=num; i++){
+        if(num % i == 0){
+            factors++;
+        }
+    }
+    return (factors>2);
+}
+int sum_composite_numbers(int n, int a[n]){
+    int sum=0;
+    for(int i=0; i<n; i++){
+        if(is_composite(a[i])){
+            sum  += a[i];
+        }
+    }
+    return sum;
+}
+void output(int sum){
+    printf("Sum of composite numbers in the array: %d\n", sum);
 }
