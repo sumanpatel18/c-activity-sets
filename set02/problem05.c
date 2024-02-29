2eb0c0f6eb5fe193ee8b3009fceb658fcdcffec8
sumanpatel18 <141306994+sumanpatel18@users.noreply.github.com>
2024-02-29 22:28:04 +0530

    committed 2/29/2024, 10:28:04 PM

---
+++ set02/problem05.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int input();
+int find_gcd(int a, int b);
+void output(int a, int b, int gcd);
+int main(){
+
+}
+int input(){
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    return n;
+}
+int find_gcd(int a, int b){
+    
+}
