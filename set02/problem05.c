031d5519b73f0963b8685bc478a281c737c2165d
sumanpatel18 <141306994+sumanpatel18@users.noreply.github.com>
2024-03-01 17:38:38 +0530

    committed 3/1/2024, 5:38:41 PM

--- set02/problem05.c
+++ set02/problem05.c
@@ -3,7 +3,11 @@ int input();
 int find_gcd(int a, int b);
 void output(int a, int b, int gcd);
 int main(){
-
+int num1 = input();
+int num2 = input();
+int gcd = find_gcd(num1, num2);
+output(num1, num2, gcd);
+return 0;
 }
 int input(){
     int n;
@@ -12,5 +16,13 @@ int input(){
     return n;
 }
 int find_gcd(int a, int b){
-    
-}
+    while(b!=0){
+        int temp=b;
+        b= a% b;
+        a=temp;
+    }
+    return a;
+    }
+    void output(int a, int b, int gcd){
+        printf("GCD of %d and %d is: %d\n", a, b, gcd);
+    }
